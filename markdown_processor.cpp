932713e3b4c3f931f925f81eeb7e4cb8bdc8580a
Sergey Veneckiy <s.venetsky@gmail.com>
2024-09-15 19:59:03 +0300

    Single # comlete

--- markdown_processor.cpp
+++ markdown_processor.cpp
@@ -3,8 +3,8 @@
 QString MarkdownProcessor::processHeader(const QString& line) {
     if (line.startsWith("# ")) {
         QString content = line.mid(2).trimmed();
-        return QString("<h1 style='font-size: 2em; font-weight: 600; padding-bottom: 0.3em; margin-bottom: 16px;'>%1</h1>"
-                       "<hr style='height: 0.25em; padding: 0; margin: 24px 0; background-color: #e1e4e8; border: 0;'>")
+        return QString("<h1 style='font-size: 2em; font-weight: 600; padding-bottom: 0.3em; margin-bottom: 0;'>%1</h1>"
+                       "<hr style='height: 0.25em; padding: 0; margin: 0 0 24px 0; background-color: #e1e4e8; border: 0;'>")
             .arg(content);
     }
     return line;
