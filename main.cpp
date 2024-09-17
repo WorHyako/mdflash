39d20d8eb5bd304cfcf5ddd0b036308f6772431f
Sergey Veneckiy <s.venetsky@gmail.com>
2024-09-17 12:53:40 +0300

    style: Improve appearance of main window

--- main.cpp
+++ main.cpp
@@ -2,48 +2,60 @@
 #include <QMainWindow>
 #include <QTextEdit>
 #include <QVBoxLayout>
+#include <QHBoxLayout>
 #include <QFileDialog>
 #include <QMenuBar>
 #include <QFont>
 #include <QFile>
 #include <QTextStream>
+#include <QFileInfo>
 #include "markdown_processor.h"
 
+// MarkdownViewer class: Main window for the markdown viewer application
 class MarkdownViewer : public QMainWindow {
 public:
-    MarkdownViewer() {
+    MarkdownViewer() : currentFileName("") {
         setWindowTitle("mdflash - Markdown Viewer");
-        
+
+        // Set up the central widget and main layout
         QWidget *centralWidget = new QWidget(this);
         setCentralWidget(centralWidget);
-        
-        QVBoxLayout *layout = new QVBoxLayout(centralWidget);
-        
+        QVBoxLayout *mainLayout = new QVBoxLayout(centralWidget);
+        mainLayout->setContentsMargins(20, 20, 20, 20);  // Add white margins around the content
+
+        // Create and set up the text edit for displaying markdown content
         textEdit = new QTextEdit(this);
         textEdit->setReadOnly(true);
         textEdit->setFont(QFont("Arial", 12));
-        layout->addWidget(textEdit);
-        
+        textEdit->setFrameStyle(QFrame::NoFrame);
+        textEdit->setStyleSheet("QTextEdit { background-color: white; color: #24292e; }");
+        mainLayout->addWidget(textEdit);
+
         createMenus();
-        
-        resize(800, 600);
+        setupStyle();
+
+        resize(1000, 800);  // Set initial window size
     }
 
+    // Open and display the content of a markdown file
     void openFile(const QString &fileName) {
-    if (!fileName.isEmpty()) {
-        QFile file(fileName);
-        if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-            QTextStream in(&file);
-            in.setCodec("UTF-8");
-            QString content = in.readAll();
-            textEdit->setHtml(MarkdownProcessor::processContent(content));
-            file.close();
+        if (!fileName.isEmpty()) {
+            QFile file(fileName);
+            if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+                QTextStream in(&file);
+                in.setCodec("UTF-8");
+                QString content = in.readAll();
+                textEdit->setHtml(MarkdownProcessor::processContent(content));
+                file.close();
+
+                QFileInfo fileInfo(fileName);
+                currentFileName = fileInfo.fileName();
+            }
         }
     }
-}
-
 
 private:
+    // Create the application menu
     void createMenus() {
         QMenu *fileMenu = menuBar()->addMenu("File");
 
@@ -56,19 +68,44 @@ private:
         fileMenu->addAction(exitAction);
     }
 
+    // Open a file dialog to select a markdown file
     void openFileDialog() {
         QString fileName = QFileDialog::getOpenFileName(this, "Open Markdown File", "", "Markdown Files (*.md);;All Files (*)");
         openFile(fileName);
     }
 
+    // Set up the application's visual style
+    void setupStyle() {
+        setStyleSheet(
+            "QMainWindow {"
+            "   background-color: #ffffff;"
+            "}"
+            "QMenuBar {"
+            "   background-color: #f6f8fa;"
+            "   border-bottom: 1px solid #d0d7de;"
+            "}"
+            "QMenuBar::item {"
+            "   padding: 5px 10px;"
+            "   background-color: transparent;"
+            "}"
+            "QMenuBar::item:selected {"
+            "   background-color: #0366d6;"
+            "   color: white;"
+            "}"
+        );
+    }
+
     QTextEdit *textEdit;
+    QString currentFileName;
 };
 
+// Main function: Initialize and run the application
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     MarkdownViewer viewer;
     viewer.show();
 
+    // If a file path is provided as a command-line argument, open it
     if (argc > 1) {
         viewer.openFile(QString::fromLocal8Bit(argv[1]));
     }
